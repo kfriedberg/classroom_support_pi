679a166c5f9c0ad405d141b7c02e51775adf08c2
kfriedberg <kfriedberg@users.noreply.github.com>
2019-04-16 02:05:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt-get -y install unclutter
 
 ln -s autostart /home/pi/.config/lxsession/LXDE-pi
@@ -9,8 +11,11 @@ ln -s 10-nopasswd_pi_reboot.rules /etc/polkit-1/rules.d/10-nopasswd_pi_reboot.ru
 ln -s 020_pi-passwd-override /etc/sudoers.d/020_pi-passwd-override
 
 echo */5 * * * * pi <currentdir>/sendmac.sh > classroom_support-cronjob
+chmod a+x sendmac.sh
 
 ln -s classroom_support-cronjob /etc/cron.d/classroom_support-cronjob
+
+chown root: *
 
 systemctl enable ssh
 systemctl start ssh
