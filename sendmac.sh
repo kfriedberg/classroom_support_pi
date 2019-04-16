61c363beea61d3e48259e224012d90dc018cde6e
kfriedberg <kfriedberg@users.noreply.github.com>
2019-04-16 15:02:41 -0400

    Update sendmac.sh

--- sendmac.sh
+++ sendmac.sh
@@ -5,4 +5,4 @@
 MAC=$(ip -o link show dev eth0 | grep -Po 'ether \K[^ ]*')
 IP=$(hostname -I)
 echo "$MAC"
-curl "$HOST/webservice/rest/server.php" -d"wstoken=$TOKENwsfunction=sshd_get_raspberry_pi&mac=$MAC&ip=$IP"
+curl "$HOST/webservice/rest/server.php" -d"wstoken=$TOKEN&wsfunction=sshd_get_raspberry_pi&mac=$MAC&ip=$IP"
