0d834036dafadba8a50b1e3bbe8e558cda616e01
kfriedberg <kfriedberg@users.noreply.github.com>
2019-04-16 15:07:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ ln -sf $CURRENTDIR/10-nopasswd_pi_reboot.rules /etc/polkit-1/rules.d/10-nopasswd
 
 ln -sf $CURRENTDIR/020_pi-passwd-override /etc/sudoers.d/020_pi-passwd-override
 
-echo */5 * * * * pi $CURRENTDIR/sendmac.sh > classroom_support-cronjob
+echo "*/5 * * * * pi $CURRENTDIR/sendmac.sh" > classroom_support-cronjob
 chmod a+x sendmac.sh
 
 ln -sf $CURRENTDIR/classroom_support-cronjob /etc/cron.d/classroom_support-cronjob
