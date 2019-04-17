5b51987b520c29046b153e474020b84649866626
kfriedberg <kfriedberg@users.noreply.github.com>
2019-04-16 21:54:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,25 +6,25 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   SOURCE="$(readlink "$SOURCE")"
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-CURRENTDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+SOURCEDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 apt-get -y install unclutter
 
 mkdir -p /home/pi/.config/lxsession/LXDE-pi
-ln -sf $CURRENTDIR/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
+ln -sf $SOURCEDIR/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
 
 mkdir -p /etc/polkit-1/localauthority/50-local.d
-ln -sf $CURRENTDIR/10-nopasswd_pi_reboot.pkla /etc/polkit-1/localauthority/50-local.d/10-nopasswd_pi_reboot.pkla
+ln -sf $SOURCEDIR/10-nopasswd_pi_reboot.pkla /etc/polkit-1/localauthority/50-local.d/10-nopasswd_pi_reboot.pkla
 
 mkdir -p /etc/polkit-1/rules.d
-ln -sf $CURRENTDIR/10-nopasswd_pi_reboot.rules /etc/polkit-1/rules.d/10-nopasswd_pi_reboot.rules
+ln -sf $SOURCEDIR/10-nopasswd_pi_reboot.rules /etc/polkit-1/rules.d/10-nopasswd_pi_reboot.rules
 
-ln -sf $CURRENTDIR/020_pi-passwd-override /etc/sudoers.d/020_pi-passwd-override
+ln -sf $SOURCEDIR/020_pi-passwd-override /etc/sudoers.d/020_pi-passwd-override
 
 echo "*/1 * * * * root /bin/bash $CURRENTDIR/sendmac.sh" > classroom_support_cronjob
 chmod a+x sendmac.sh
 
-ln -sf $CURRENTDIR/classroom_support_cronjob /etc/cron.d/classroom_support_cronjob
+ln -sf $SOURCEDIR/classroom_support_cronjob /etc/cron.d/classroom_support_cronjob
 
 chown root: *
 
