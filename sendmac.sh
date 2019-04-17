592907ff85d7ef359f77f26435134e37f9cd33a8
kfriedberg <kfriedberg@users.noreply.github.com>
2019-04-16 23:26:59 -0400

    Update sendmac.sh

--- sendmac.sh
+++ sendmac.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-[[ -f classroom_support_pi.conf ]] && . classroom_support_pi.conf || echo "classroom_support_pi.conf not found, please copy it from classroom_support_pi.conf.example"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SOURCEDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+[[ -f $SORUCEDIR/classroom_support_pi.conf ]] && . $SOURCEDIR/classroom_support_pi.conf || echo "classroom_support_pi.conf not found, please copy it from classroom_support_pi.conf.example"
 
 MAC=$(ip -o link show dev eth0 | grep -Po 'ether \K[^ ]*')
 IP=$(hostname -I)
