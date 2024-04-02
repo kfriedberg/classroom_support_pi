c637f39bc359470eaf9ef09874d79a8b9650783d
Kevin Friedberg <kfriedberg@users.noreply.github.com>
2024-04-01 23:44:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,6 +50,7 @@ systemctl start ssh
 if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
   apt-get -y install crudini
   crudini --inplace --set --ini-options=nospace ~/.config/wf-panel-pi.ini panel updater_interval 0
+  killall -SIGHUP wf-panel-pi
 else
   if [ ! -f /home/$SUDO_USER/.config/lxpanel/LXDE-pi/panels/panel ]; then
     mkdir /home/$SUDO_USER/.config/lxpanel/LXDE-pi/panels
